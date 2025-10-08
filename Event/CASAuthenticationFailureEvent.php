935f1d6f2cdf51b595621f5e2e03e47ae62f65e5
Eduard Morcinek <eduard.morcinek@1und1.de>
2025-10-08 14:35:21 +0200

    fix!: use correct namespace for Event class
    BREAKING CHANGE: drop Symfony 3 support

--- Event/CASAuthenticationFailureEvent.php
+++ Event/CASAuthenticationFailureEvent.php
@@ -5,8 +5,7 @@ namespace PRayno\CasAuthBundle\Event;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
-use Symfony\Component\EventDispatcher\Event;
-
+use Symfony\Contracts\EventDispatcher\Event;
 
 class CASAuthenticationFailureEvent extends Event {
 
